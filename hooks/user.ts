58c5058a5ba34ede619a961497c769bc8d30b40f
ladell.erby <ladellerby@techsavagery.net>
2021-02-26 03:13:12 -0800

    - Added findAllUsers function in users repository - Removed emil from  extractUser parameters - Update PostCard componenet to return creators name. - update variable names in user hooks - add data to the user interface - Added GET /api/users/[userId] endpoint - Added GET /api/users endpoint - Added /users/[userId] profile page

--- hooks/user.ts
+++ hooks/user.ts
@@ -3,13 +3,13 @@ import fetcher from "../lib/fetch";
 
 export function useCurrentUser() {
   const { data, mutate } = useSWR("/api/user", fetcher);
-  const user = data?.user;
-  return [user, { mutate }];
+  const userData = data?.user;
+  return [userData, { mutate }];
 }
 
 export function useUser(id: string) {
   const { data } = useSWR(`/api/user/${id}`, fetcher, {
     revalidateOnFocus: false,
   });
-  return data?.user;
+  return data;
 }
